189bd809cbd036430b18b1b4b054e131ea2ee57e
rohan.choudhary <rohan.choudhary@99acres.com>
2020-08-29 12:54:41 +0530

    MERGE : boildercode for react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() { 
+        return (<div>Hello world! I am Rohan...?
+
+        </div>  );
+    }
+}
+ 
+export default App;
